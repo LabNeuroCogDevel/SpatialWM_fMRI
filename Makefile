5eb976e80066374524ebf3eddcde239b266ecd3c
Will Foran <willforan@gmail.com>
2014-11-17 13:44:45 -0500

    update genDly to be usable

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
-all: trialdata
+all: dlydur
 
 trialdata: 
 	./00_EprimeLogToLargeTxt.bash
 	# creates: alltimes_withExperiment.txt subjRuns.txt
+dlydur: trialdata
+	R --vanilla < genDlyForMarry.R # generates files for dly1d/*
--- genDlyForMarry.R
+++ genDlyForMarry.R
@@ -1,6 +1,8 @@
-# DO NOT USE (WF:20141117)
-#  does not know about "Experiment" (i.e. run#). Guesses based on SessionTime. This is inadequate.
-# 
+# use alltimes_withExperiment to write delay duration times for each subject
+# output in dly1d with run per line, * if no run
+# N.B. code here confirms all runs of the same run number have identical sequence of cue delays
+#     that is, delay durations for run 3 of 11196 == run 3 of 10841 
+
 
 library(plyr)
 df<-read.table('alltimes_withExperiment.txt',header=T,sep="\t")
@@ -16,13 +18,6 @@ l <-
   })
 # l[["10662"]][["1"]]
 
-# look at all run 1s
-run <- sapply(l,'[[','4')
-run[ sapply(run,is.null) | sapply(run,length)!=36 ] <- NULL # remove nulls and weird runs
-# see how many have the same delay length for that trial
-#apply(run1s,1,function(x){r<-rle(sort(x)); rbind(r$values,r$lengths)})
-tab <-sapply(run,unlist) 
-apply(tab,1,function(x){r<-rle(sort(x));rbind(r$values,r$lengths)})
 
 # write out files
 for(subj in names(l)){
@@ -31,11 +26,32 @@ for(subj in names(l)){
 
  # run through each run, put dlys or *
  for(run in c("1","2","3","4") ){
-   rundly<-l[[subj]][[run]]
-   cat(ifelse(rundly,rundly,'*'),"\n")
+     rundly<-l[[subj]][[run]]
+     if(is.null(rundly)){
+         rundly<-'*'
+     } else {
+         rundly<-rundly/1000
+     }
+     cat( rundly, "\n" )
  }
  # close file
  sink()
 }
 
+## write out generic dly dur file
+for( r in c("1","2","3","4") ){
+
+    # look at all run 1s
+    run <- sapply(l,'[[',r)
+    run[ sapply(run,is.null) | sapply(run,length)!=36 ] <- NULL # remove nulls and weird runs
+    # see how many have the same delay length for that trial
+    #apply(run1s,1,function(x){r<-rle(sort(x)); rbind(r$values,r$lengths)})
+    tab <- apply(sapply(run,unlist),1,function(x){r<-rle(sort(x));rbind(r$values,r$lengths)})
+    if(length(unique(tab[2,]))!=1L)
+        error('not all subjects of run %d have the same delay duration sequence!',r)
+
+    sink(paste(sep="",'dly1d/RUN',r,'.1D'))
+    cat(tab[1,]/1000,"\n")
+    sink()
+ }
 
